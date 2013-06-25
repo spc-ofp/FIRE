912e0de1f4385fa0bfe3559eb1cd10a14c3a51de
Bruno <bruno.deprez@gmail.com>
2013-06-26 10:22:28 +1100

    Added group by list for flags

--- Tufces.Web/Models/KernelViewModel.cs
+++ Tufces.Web/Models/KernelViewModel.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Tufces.Web.Models
 {
     public class KernelViewModel
     {
         public List<String> Flags { get; set; }
+        public List<SelectListItem> FlagsGroupingList { get; set; }
+        public String FlagsGrouping { get; set; }
+
+        public KernelViewModel()
+        {
+            FlagsGroupingList = new List<SelectListItem>();
+            FlagsGroupingList.Add(new SelectListItem() { Value = "AllFlag", Text = "All flags combined" });
+            FlagsGroupingList.Add(new SelectListItem() { Value = "Flag", Text = "Flag" });
+        }
     }
 }
